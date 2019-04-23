76365fae43c89ce08165ba7718f67bc27877fff4
michealborn <499190328@qq.com>
2019-04-24 00:30:05 +0800

    Test  排他网关

---
+++ magfin-ccs-web/src/test/java/com/magfin/web/com/activiti/gateway/GateWayTest.java
@@ -0,0 +1,63 @@
+package com.magfin.web.com.activiti.gateway;
+
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngines;
+import org.activiti.engine.repository.Deployment;
+import org.activiti.engine.repository.DeploymentBuilder;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Time 2019/4/24
+ * @Author zlian
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GateWayTest {
+    ProcessEngine processEngine = null;
+    @Before
+    public void before(){
+        processEngine = ProcessEngines.getDefaultProcessEngine();
+    }
+
+
+    /**
+     * 部署带有排他网关的流程
+     */
+    @Test
+    public void test1(){
+        DeploymentBuilder deployment = processEngine.getRepositoryService().createDeployment();
+        //方式一：读取单个的流程定义文件
+        deployment.addClasspathResource("processes/PaiTaGateWay.bpmn");
+        deployment.addClasspathResource("processes/PaiTaGateWay.png");
+        Deployment deploy = deployment.deploy();
+    }
+
+
+    /**
+     * 启动流程
+     */
+    @Test
+    public void test2(){
+        ProcessInstance paitagateway = processEngine.getRuntimeService().startProcessInstanceByKey("paitagateway");
+        System.out.println(paitagateway.getId());
+    }
+
+
+    /**
+     * 办理任务，设置流程变量
+     */
+    @Test
+    public void test3(){
+        Map<String, Object> variables = new HashMap<>();
+        variables.put("bxje",800);
+        processEngine.getTaskService().complete("72504",variables);
+    }
+}
